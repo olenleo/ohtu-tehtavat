78efe9bee5ef5d591223c99dfad2062ef5ed28a7
Leo Niemi <niemi.leo@gmail.com>
2021-03-12 15:15:27 +0200

    Lisätty interfacet yms

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KirjanpitoInterface.java
@@ -0,0 +1,9 @@
+package  ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface KirjanpitoInterface {
+    
+    public void lisaaTapahtuma(String tapahtuma);
+    public ArrayList<String> getTapahtumat();
+}
